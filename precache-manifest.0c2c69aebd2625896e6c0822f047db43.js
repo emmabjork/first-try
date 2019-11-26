dd5fe7afe054467c15ffe2061b4f1ca862510ac7
emmabjork <56337484+emmabjork@users.noreply.github.com>
2019-11-26 20:25:31 +0100

    Updates

---
+++ precache-manifest.0c2c69aebd2625896e6c0822f047db43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413fdcaaf97601a815b9b5ea987507cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d865bf18a32aca9c4c23",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5eead37c1a866ac6416a",
+    "url": "/static/js/2.6bf7d5a3.chunk.js"
+  },
+  {
+    "revision": "d865bf18a32aca9c4c23",
+    "url": "/static/js/main.475f23e2.chunk.js"
+  },
+  {
+    "revision": "885cfd30d18011706eda",
+    "url": "/static/js/runtime-main.23490da8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
